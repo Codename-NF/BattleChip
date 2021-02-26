0b7e75dcbbad18b89368a381f236c92ccf7d12d9
Neo Tsai <neotsai@alumni.ubc.ca>
2021-02-25 17:56:20 -0800

    Have implemented set of boxes. Changing int to bool

--- software/gamelogic.cpp
+++ software/gamelogic.cpp
@@ -2,13 +2,30 @@
 #include <list>
 #include <string>
 #include <sys/socket.h>
+#include <set> 
 
 using namespace std;
-
+/**
+ * 
+ * fyi setupvalues only work for a single ship, i'm guessing you're expecting bluetooth to pass in a list of those?
+ * Why don't you just make it contain a ship anyways...
+ * 
+ * 
+ * also main really shouldn't be 150 lines
+ * you should split loops into their own functions
+ * 
+ * 
+ * also is it possible to use set of boxes instead of lists
+ * sets = O(1) lookup with no loops
+ * like not_hit_yet could be so much easier instead of requiring a loop
+ * 
+ * */
 const int NUM_OF_SHIPS = 1;
 const int SUNK_STATUS_CODE = 2;
 const int HIT_STATUS_CODE = 1;
 const int MISS_STATUS_CODE = 0;
+const int BOUNDARY_MIN = 0;
+const int BOUNDARY_MAX = 9;
 
 enum orientations {VERTICAL = 1, HORIZONTAL = 2};
 
@@ -27,6 +44,13 @@ struct box {
             this->x = x;
             this->y = y;
             this->status = status;
+        }
+
+        bool operator<(const box& rhs) const
+        {
+            int score = (this->x)*(BOUNDARY_MAX + 1) + this->y;
+            int score_rhs = (rhs.x)*(BOUNDARY_MAX + 1) + rhs.y;
+            return score < score_rhs;
         }
 } ;
 
@@ -54,6 +78,7 @@ struct player {
         list<ship> ships_list;
         list<box> boxes_hit;
         int remaining_ships;
+        set<box> all_boxes_on_board;
 
         player(int player_num) {
             for (int i = 0; i < NUM_OF_SHIPS; i++) {
@@ -62,6 +87,7 @@ struct player {
             this->player_name = "";
             this->player_num = player_num;
             this->remaining_ships = NUM_OF_SHIPS;
+            this->all_boxes_on_board = {};
         }
 } ;
 
@@ -112,8 +138,8 @@ setupvalues ask_for_setup(int player_num);
 shootvalues ask_for_shoot(int player_num);
 // ******************* bluetooth place holder *******************
 void declare_win (int player_who_won);
-int check_in_bound(int x_start, int y_start, int size, int orientation); 
-int check_path_empty(int x_start, int y_start, int size, int orientation, list<ship> ships);
+bool out_of_bound(int x_start, int y_start, int size, int orientation); 
+bool path_empty(int x_start, int y_start, set<box> all_boxes_on_board);
 bool contains_box(ship *ship, int x, int y);
 int check_hit_what(int x, int y, list<ship> *ships, int *remaining_ships);
 bool all_ships_destroyed(list<ship> ships);
@@ -195,30 +221,44 @@ int main () {
             came_from_player1 = false;
         }
 // ******************* bluetooth place holder *******************
-
-        int err;
-        err = check_in_bound(x_in, y_in, length, orientation);
         //TODO: set up error needs to be handled more carefully to makesure all ships are in place when finished.
-        if (err) {
+        if (out_of_bound(x_in, y_in, length, orientation)) {
             cout << "out of bound" << endl;
+            p1count--;
             continue;
         }
         
-        list<player>::iterator currently_setting_up = came_from_player1 ? p1: p2;
+        
+        list<player>::iterator *currently_setting_up = came_from_player1 ? &p1: &p2;
         list<ship>::iterator *ships_being_set_up = came_from_player1 ? &shipsp1: &shipsp2;
 
-        err = check_path_empty(x_in, y_in, length, orientation, currently_setting_up->ships_list);
-
-        if (err) {
-            cout << "another ship in the way for player 1 " << endl;
+        if (!path_empty(x_in, y_in, (*currently_setting_up)->all_boxes_on_board)) {
+            cout << "another ship in the way for " << (*currently_setting_up)->player_name << endl;
+            p1count--;
             continue;
         }
         
+        // setting the ship
         if ((*ships_being_set_up)->size == 0) {
             (*ships_being_set_up)->orientation = orientation;
             (*ships_being_set_up)->size = length;
             (*ships_being_set_up)->start_box = box(x_in, y_in);
             (*ships_being_set_up)++;
+        }
+
+        int offset_x, offset_y;
+        if (orientation == VERTICAL) {
+            offset_x = 0;
+            offset_y = 1;
+        }
+        else {
+            offset_x = 1;
+            offset_y = 0;
+        }
+
+        // adding all the boxes into the set
+        for (int i = 0; i < length; i++) {
+            (*currently_setting_up)->all_boxes_on_board.insert(box(x_in + (offset_x * i), y_in + (offset_y * i)));
         }
 
         if (shipsp1 == p1->ships_list.end() && shipsp2 == p2->ships_list.end()) {
@@ -298,85 +338,20 @@ void declare_win (int player_who_won) {
     cout << "Player number " << player_who_won << " won" << endl;
 }
 
-int check_in_bound(int x_start, int y_start, int size, int orientation) {
-    if (x_start < 0 || y_start < 0 || x_start > 9 || y_start > 9) {
-        return 1;
+bool out_of_bound(int x_start, int y_start, int size, int orientation) {
+    if (x_start < BOUNDARY_MIN || y_start < BOUNDARY_MIN || x_start > BOUNDARY_MAX || y_start > BOUNDARY_MAX) {
+        return true;
     }
-    switch (orientation) {
-        case VERTICAL:
-            if (y_start + (size - 1) > 9 ) {
-                return 1;
-            }
-            break;
-        case HORIZONTAL:
-            if (x_start + (size - 1) > 9 ) {
-                return 1;
-            }
-            break;
-        default:
-            cout << "What else direction can you have?" << endl;
-            break;
+    int axis_value = orientation == VERTICAL ? y_start : x_start;
+    if (axis_value + (size - 1) > BOUNDARY_MAX) {
+        return true;
     }
-
+    
     // only get here if no error
-    return 0;
+    return false;
 }
-int check_path_empty(int x_start, int y_start, int size, int orientation, list<ship> ships) {
-    for (list<ship>::iterator it=ships.begin(); it != ships.end(); it++ ) {
-        if ( it->size != 0 ) {
-            if ( orientation == VERTICAL && it->orientation == VERTICAL ) {
-                // if both vertical
-                if (x_start != it->start_box.x) {
-                        return 0;
-                }
-                else {
-                    // if on the same line 
-                    if (y_start + size - 1 >= it->start_box.y) {
-                       return 1;
-                    }
-                    else {
-                        return 0;
-                    }
-                } 
-            }
-            else if ( orientation == HORIZONTAL && it->orientation == HORIZONTAL ) {
-                // if both horizontal
-                if (y_start != it->start_box.y) {
-                    return 0;
-                }
-                else {
-                    // if on the same line 
-                    if (x_start + size - 1 >= it->start_box.x) {
-                        return 1;
-                    }
-                    else {
-                        return 0;
-                    }
-                }
-            }
-            else if ( orientation == HORIZONTAL && it->orientation == VERTICAL ) {
-                for (int i = 0; i < size; i++) {
-                    for (int j = 0; j < it->size; j++) {
-                        if ( it->start_box.y + j == y_start && it->start_box.x == x_start + i) {
-                            return 1;
-                        }
-                    }
-                }
-            }
-            else if ( orientation == VERTICAL && it->orientation == HORIZONTAL ) {
-                for (int i = 0; i < size; i++) {
-                    for (int j = 0; j < it->size; j++) {
-                        if ( it->start_box.x + j == x_start && it->start_box.y == y_start + i) {
-                            return 1;
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    // reaches here if nothing is wrong
-    return 0;
+bool path_empty(int x_start, int y_start, set<box> all_boxes_on_board) {
+    return all_boxes_on_board.find(box(x_start, y_start)) == all_boxes_on_board.end();
 }
 
 bool contains_box(ship *ship, int x, int y) {
