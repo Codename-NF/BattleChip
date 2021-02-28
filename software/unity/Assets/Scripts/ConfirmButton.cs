a21510da8f9fcd2c5d5d3171ff52c4b5763ef1fa
thomaslapadat <thomaslapadat@gmail.com>
2021-02-28 14:22:17 -0800

    Forgot to add ConfirmButton script to last commit

---
+++ software/unity/Assets/Scripts/ConfirmButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ConfirmButton : MonoBehaviour
+{
+    public Board mBoard;
+    public PieceManager mPieceManager;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Button>().interactable = false;
+    }
+
+    void FixedUpdate()
+    {
+        GetComponent<Button>().interactable = (mPieceManager.mNoActiveInteraction && mBoard.ValidateShips());
+    }
+}
