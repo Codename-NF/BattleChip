eec70a95128cb9c8c441c014f24d6c8b9dc74998
Paul Lin <linpaul0328@gmail.com>
2021-02-27 23:32:25 -0800

    Define Unity/Android interfaces and add Retrofit
    * Functions for passing messages between Unity and Android defined
    * App can retrieve user information from database server using Retrofit

--- software/unity/Assets/Scripts/GameManager.cs
+++ software/unity/Assets/Scripts/GameManager.cs
@@ -10,7 +10,23 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log(name);
+        Debug.Log(mBoard + " " + mPieceManager);
         mBoard.Create();
         mPieceManager.Setup(mBoard);
     }
+
+    void ReceiveBluetoothMessageFromConsole(string message) {
+        Debug.Log("Received: " + message);
+        // TODO: REPLACE THIS WITH ACTUAL PROCESSING
+        SendBluetoothMessageToConsole(message);
+    }
+
+    void SendBluetoothMessageToConsole(string message) {
+        Debug.Log("Sending: " + message);
+        AndroidJavaClass jc = new AndroidJavaClass("com.nf.battlechip.MainUnityActivity");
+        AndroidJavaObject unityActivity = jc.GetStatic<AndroidJavaObject>("instance");
+        unityActivity.Call("sendBluetoothMessageToConsole", message);
+    }
+
 }
