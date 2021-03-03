4a2bdaccb2bab1cc0e6611eb0ebbb036e8f5bd8b
thomaslapadat <thomaslapadat>
2021-03-03 08:20:39 +0000

    Finished implementing Google OAuth2 on the server

--- software/cloud/middleware/auth.js
+++ software/cloud/middleware/auth.js
@@ -3,39 +3,29 @@ const googleAuth = require('../plugins/google-auth');
 // Function called by middleware to verify that the google token is valid
 module.exports = {
     checkToken: async (req, res, next) => {
-        res.locals.userId = "123456789"
-        res.locals.email = "paul@lin.com";
-        res.locals.firstname = "Paul";
-        res.locals.lastname = "Lin";
-        next();
+        if (req.headers.id_token) {
+            const token = req.headers.id_token;
+
+            // Verify the token
+            const credentials = await googleAuth.checkGoogle(token);
+            console.log(credentials);
+            if (credentials) {
+                // Store credentials
+                res.locals.userId = credentials.userId;
+                res.locals.email = credentials.email;
+                res.locals.firstname = credentials.given_name;
+                res.locals.lastname = credentials.family_name;
+
+                // Grant access to endpoint
+                next();
+            } else {
+                // Bad user
+                res.status(403).send('Bad Token.');
+            }
+        } else {
+            // No token
+            res.status(401).send('Token missing.');
+        }
     }
 };
-    /*
-    const bearerHeader = req.headers['ID-Token'];
-
-    if (bearerHeader) {
-        // Isolate the token from the header
-        const bearer = bearerHeader.split(' ');
-        const bearerToken = bearer[1];
-        req.token = bearerToken;
-
-        // Verify the token
-        const credentials = await googleAuth(req.token);
-        if (credentials) {
-            // Store credentials
-            res.locals.userId = credentials.userId;
-            res.locals.email = credentials.email;
-            res.locals.firstname = credentials.given_name;
-            res.locals.lastname = credentials.family_name;
-
-            // Grant access to endpoint
-            next();
-        } else {
-            // Bad user
-            res.status(403).send('Bad Token.');
-        }
-    } else {
-        // No token
-        res.status(401).send('Token missing.');
-    }
-    */
+    
--- software/cloud/plugins/google-auth.js
+++ software/cloud/plugins/google-auth.js
@@ -1,24 +1,25 @@
 const { OAuth2Client } = require('google-auth-library');
 const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 
-const googleAuth = async (token) => {
-    // Attempt to verify the token
-    let ticket = null;
-    try {
-        ticket = await client.verifyIdToken({
-            idToken: token,
-            audience: process.env.GOOGLE_CLIENT_ID,
-        });
-    } catch (err) {
-        return null;
+module.exports = {
+    checkGoogle: async (token) => {
+        // Attempt to verify the token
+        let ticket = null;
+        try {
+            ticket = await client.verifyIdToken({
+                idToken: token,
+                audience: process.env.GOOGLE_CLIENT_ID,
+            });
+        } catch (err) {
+            console.log(err);
+            return null;
+        }
+
+        // Token is good, so extract the credentials
+        const payload = ticket.getPayload();
+        const { sub, email, given_name, family_name } = payload;
+        const userId = sub;
+
+        return { userId, email, given_name, family_name };
     }
-
-    // Token is good, so extract the credentials
-    const payload = ticket.getPayload();
-    const { sub, email, given_name, family_name } = payload;
-    const userId = sub;
-
-    return { userId, email, given_name, family_name };
-};
-
-module.exports = googleAuth;
+}
