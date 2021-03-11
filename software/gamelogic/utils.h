8717846c7dd09850c60da5bc62633f16fd3fbbaf
neotsai0622 <neotsai@alumni.ubc.ca>
2021-03-10 21:04:14 -0800

    Have new field in player to hold the 5 bit one hot representation of ships alive and change that inside check_hit_what

--- software/gamelogic/utils.h
+++ software/gamelogic/utils.h
@@ -5,12 +5,13 @@
 #include <sys/socket.h>
 #include "constants.h"
 #include "structs.h"
+#include <bits/stdc++.h> 
 
 void declare_win (int player_who_won);
 bool out_of_bound(int x_start, int y_start, int size, int orientation); 
 bool path_empty(int x_start, int y_start, set<box> all_boxes_on_board);
 bool contains_box(ship *ship, int x, int y);
-int check_hit_what(int x, int y, list<ship> *ships, int *remaining_ships);
+int check_hit_what(int x, int y, list<ship> *ships, int *remaining_ships, bitset<5> *ships_alive);
 bool not_hit_yet(int x, int y, set<box> boxes);
 
 void change_status_box_all_boxes(int x, int y, set<box> *boxes_hit, list<ship> *ships);
