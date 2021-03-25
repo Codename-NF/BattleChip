58f3857b92a1f44489129a597b6bb79743f3f9c5
thomaslapadat <thomaslapadat@gmail.com>
2021-03-25 15:18:39 -0700

    Added dynamic background to UI

--- software/unity/Assets/Scripts/PauseMenu.cs
+++ software/unity/Assets/Scripts/PauseMenu.cs
@@ -40,6 +40,8 @@ public class PauseMenu : MonoBehaviour
     public void Forfeit()
     {
         Debug.Log("Forfeiting Game");
+        AndroidJavaClass jc = new AndroidJavaClass("com.nf.battlechip.UnityMessage");
+        jc.CallStatic("forfeit", "thisParamMightNotExist");
         Application.Quit();
     }
 }
