64de3353c4bd254cb3e3db9000c311a9ffa06680
mslongo <mslongo@live.ca>
2021-03-29 21:58:05 -0700

    C testing files.

---
+++ hardware/Graphics.h
@@ -0,0 +1,61 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+#include <stdio.h>
+
+// graphics register addresses
+
+#define GraphicsCommandReg   		(*(volatile unsigned short int *)(0xFF210000))
+#define GraphicsStatusReg   		(*(volatile unsigned short int *)(0xFF210000))
+#define GraphicsX1Reg   			(*(volatile unsigned short int *)(0xFF210002))
+#define GraphicsY1Reg   			(*(volatile unsigned short int *)(0xFF210004))
+#define GraphicsX2Reg   			(*(volatile unsigned short int *)(0xFF210006))
+#define GraphicsY2Reg   			(*(volatile unsigned short int *)(0xFF210008))
+#define GraphicsColourReg   		(*(volatile unsigned short int *)(0xFF21000E))
+#define GraphicsBackGroundColourReg   	(*(volatile unsigned short int *)(0xFF210010))
+
+/************************************************************************************************
+** This macro pauses until the graphics chip status register indicates that it is idle
+***********************************************************************************************/
+
+#define WAIT_FOR_GRAPHICS		while((GraphicsStatusReg & 0x0001) != 0x0001);
+
+// #defined constants representing values we write to the graphics 'command' register to get
+// it to draw something. You will add more values as you add hardware to the graphics chip
+// Note DrawHLine, DrawVLine and DrawLine at the moment do nothing - you will modify these
+
+#define DrawHLine		1
+#define DrawVLine		2
+#define DrawLine			3
+#define Blankboard      4
+#define Squaremapper    5
+#define	PutAPixel		0xA
+#define	GetAPixel		0xB
+#define	ProgramPaletteColour    0x10
+
+// defined constants representing colours pre-programmed into colour palette
+// there are 256 colours but only 8 are shown below, we write these to the colour registers
+//
+// the header files "Colours.h" contains constants for all 256 colours
+// while the course file "ColourPaletteData.c" contains the 24 bit RGB data
+// that is pre-programmed into the palette
+
+#define VERTICAL		1
+#define HORIZONTAL		2
+
+
+void WriteAPixel(int x, int y, int Colour);
+void blankscreen(int Colour);
+void squaremapper(int x, int y, int player, int colour);
+void squaremappership(int player, int x, int y, int length, int dir, int done, int colour);
+int ReadAPixel(int x, int y);
+void ProgramPalette(int PaletteNumber, int RGB);
+void HLine(int x1, int y1, int length, int Colour);
+void VLine(int x1, int y1, int length, int Colour);
+int abs(int a);
+int sign(int a);
+void Line(int x1, int y1, int x2, int y2, int Colour);
+
+#ifdef __cplusplus
+}
+#endif
---
+++ hardware/RS232.c
@@ -0,0 +1,130 @@
+#include "RS232.h"
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+/**************************************************************************
+/*  Subroutine to initialise the RS232 Port by writing some data
+**  to the internal registers.
+**  Call this function at the start of the program before you attempt
+**  to read or write to data via the RS232 port
+**
+**  Refer to UART data sheet for details of registers and programming
+***************************************************************************/
+
+int main(void) {
+    int i = 0;
+    // *WIFI_ENABLE = 1;
+    // for (i = 0; i < 20; i++) {
+    //     printf("1\n");
+    // }
+    Init_RS232();
+    RS232Flush();
+    *WIFI_RESET = 0;
+    for (i = 0; i < 20; i++) {
+        printf("1\n");
+    }
+    *WIFI_RESET = 1;
+    for (i = 0; i < 20; i++) {
+        printf("2\n");
+    }
+    RS232Flush();
+    char myString[3];
+    myString[2] = '\0';
+    printf("Hello\n");
+    char hello = '0';
+    putcharRS232('A');
+    putcharRS232('T');
+    putcharRS232('\r');
+    putcharRS232('\n');
+    
+    while (1) {
+                
+        printf("Sent commands... \n");
+        hello = getcharRS232();
+        if (hello == '\n') {
+            printf("Received new line\n");
+        } else if (hello == '\r') {
+            printf("Received backslash r line\n");
+        } else {
+            printf("%c\n", hello);
+        }
+        myString[0] = hello;
+        hello = getcharRS232();
+        if (hello == '\r' || hello == '\n') {
+            printf("Received new line\n");
+        } else {
+            printf("%c\n", hello);
+        }
+        myString[1] = hello;
+        if (myString[0] == 'O' && myString[1] == 'K') {
+            break;
+        }
+    }
+    printf("My string is: %s\n", myString);
+    printf("%d\n", RS232TestForReceivedData());
+    RS232Flush();
+    return 0;
+}
+
+void Init_RS232(void)
+{
+    // set bit 7 of Line Control Register to 1, to gain access to the baud rate registers
+    set_bit(RS232_LineControlReg, 7);
+    // set Divisor latch (LSB and MSB) with correct value for required baud rate
+    int divisor_value = BR_CLK / (DESIRED_BAUD_RATE * 16);
+    *RS232_DivisorLatchLSB = (divisor_value & 0xFF);
+    *RS232_DivisorLatchMSB = (divisor_value & 0xFF00) >> 8;
+    // set bit 7 of Line control register back to 0 and
+    clear_bit(RS232_LineControlReg, 7);
+    // program other bits in that reg for 8 bit data, 1 stop bit, no parity etc
+    *RS232_LineControlReg |= 0x3; // 8 bit data
+    clear_bit(RS232_LineControlReg, 2); // 1 stop bit
+    clear_bit(RS232_LineControlReg, 3); // no parity
+    // Reset the Fifo’s in the FiFo Control Reg by setting bits 1 & 2
+    *RS232_FifoControlReg |= 0x6;
+    // Now Clear all bits in the FiFo control registers
+    *RS232_FifoControlReg = 0;
+}
+
+int putcharRS232(int c)
+{
+    // wait for Transmitter Holding Register bit (5) of line status register to be '1'
+    // indicating we can write to the device
+    // write character to Transmitter fiforegister
+    // return the character we printed
+    while (!(*RS232_LineStatusReg & (1 << 5)));
+    *RS232_TransmitterFifo = c;
+    return c;
+}
+
+int getcharRS232(void)
+{
+    // wait for Data Ready bit (0) of line status register to be '1'
+    // read new character from ReceiverFiFo register
+    // return new character
+    while (!RS232TestForReceivedData());
+    return *RS232_ReceiverFifo;
+}
+
+// the following function polls the UART to determine if any character
+// has been received. It doesn't wait for one,or read it, it simply tests
+// to see if one is available to read from the FIFO
+int RS232TestForReceivedData(void)
+{
+    // if RS232_LineStatusReg bit 0 is set to 1
+    // return TRUE, otherwise return FALSE
+    return (*RS232_LineStatusReg) & 1;
+}
+
+//
+// Remove/flush the UART receiver buffer by removing any unread characters
+//
+void RS232Flush(void)
+{
+    unsigned char unwanted_char;
+    // read while bit 0 of Line Status Register == ‘1’
+    while (RS232TestForReceivedData()) {
+        unwanted_char = *RS232_ReceiverFifo;
+    }
+}
