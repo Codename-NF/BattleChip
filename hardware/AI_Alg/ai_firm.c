053bf2a9a37667ecb5e5b3f6f769099891ab6f35
Eleiah <eleiahhengeveld@gmail.com>
2021-04-04 12:00:06 -0700

    Pfft, Neo

--- hardware/AI_Alg/ai_firm.c
+++ hardware/AI_Alg/ai_firm.c
@@ -1,28 +1,5 @@
+#include "ai_firm.h"
 #include <stdio.h>
-#define ai_alg_base (volatile int *) 0xFF202040
-
-// int main()
-// {
-//     int fired = 0x00000;
-//     int hits = 0x00000;
-//     int ships = 0xFFFFFFFFFFFFFFFF;
-
-//     printf("Starting\n");
-//     *(ai_alg_base+1) = fired;
-//     *(ai_alg_base+2) = fired;
-//     printf("1\n");
-//     *(ai_alg_base+3) = hits;
-//     *(ai_alg_base+4) = hits;
-//     printf("2\n");
-//     *(ai_alg_base+5) = ships;
-//     printf("3\n");
-//     *(ai_alg_base) = 0x0; // Run command
-//     printf("4\n");
-
-//     printf("Where to shoot: %x\n", *(ai_alg_base));
-//     printf("5\n");
-//     return(0);
-// }
 
 int ai_where_to_shoot(unsigned long fired_0, unsigned long fired_1, unsigned long fired_2, unsigned long fired_3,
                     unsigned long hits_0, unsigned long hits_1, unsigned long hits_2, unsigned long hits_3, unsigned long ships)
