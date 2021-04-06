39a2ec0b3ca981ac3a17c0f7abfb39727272832f
thomaslapadat <thomaslapadat>
2021-04-06 23:41:10 +0000

    Schema validation now allows Google Users without a last name. Server now differentiates between easy and hard AI in match history

--- software/cloud/controllers/login.js
+++ software/cloud/controllers/login.js
@@ -7,12 +7,13 @@ module.exports = {
         
         // If so, send a positive response
         if (myUser) {
-            res.status(200).send({"status" : "OK"})
+            res.status(200).send(myUser)
         }
         // If not, create a new user from the token info
         else {
             // Get count of users in DB, assign unique id as count + 1
             const userCount = await User.countDocuments({});
+            const lastName = (res.locals.lastname) ? res.locals.lastname : "";
             
             const newUser = {
 				player_id: userCount, // Not userCount + 1 since id is zero indexed
@@ -24,11 +25,11 @@ module.exports = {
 			}
             try {
                 await User.create(newUser);
-                res.status(201).send({"status" : "OK"})
+                res.status(201).send(newUser)
             }
             catch (err) {
                 console.log(err)
-                res.status(500).send({"status" : "ERR"});
+                res.status(500).send({});
             }
 		}
 	}
